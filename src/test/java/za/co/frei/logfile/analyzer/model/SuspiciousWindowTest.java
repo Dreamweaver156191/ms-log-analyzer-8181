fb4de97130c795fca537bf79c9509fff25cba09c
Dreamweaver <francois156191@gmail.com>
2025-09-30 13:37:11 +0200

    test: add validation to SuspiciousWindow record

---
+++ src/test/java/za/co/frei/logfile/analyzer/model/SuspiciousWindowTest.java
@@ -0,0 +1,15 @@
+package za.co.frei.logfile.analyzer.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SuspiciousWindowTest {
+
+
+}
