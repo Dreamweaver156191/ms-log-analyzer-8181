474f715bb427b53757483e5449f84bbc5d65ea75
Dreamweaver <francois156191@gmail.com>
2025-09-30 12:04:07 +0200

    feat: Initial commit.

---
+++ src/main/java/za/co/frei/logfile/analyzer/LogfileAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package za.co.frei.logfile.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogfileAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogfileAnalyzerApplication.class, args);
+    }
+}
